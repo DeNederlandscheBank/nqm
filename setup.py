d959f9902f06ed9d09de8921413b73aa25f6b30a
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2020-11-01 13:16:06 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A LSTM-based Machine for answering questions on XBRL data',
+    author='De Nederlandsche Bank',
+    license='MIT',
+)
---
+++ src/data/__init__.py
@@ -0,0 +1,1 @@
+from .xbrl2rdf import *
