18dc4af0d6bed9a2456d907fdb146a61bfa50dc8
jm-glowienke <jmglowienke@gmail.com>
2021-03-19 10:48:39 +0100

    baseline model training

--- src_eiopa/models/baseline.py
+++ src_eiopa/models/baseline.py
@@ -197,4 +197,64 @@ def init_model( device, input_dim, output_dim,
 
     optimizer = optim.Adam(model.parameters())
 
-    print(f'The model has {count_parameters(model):,} trainable parameters')
+    print(f'Model initialized! \n The model has \
+    {count_parameters(model):,} trainable parameters')
+
+    return model,optimizer
+
+def train(model: nn.Module,
+          iterator: torch.utils.data.DataLoader,
+          optimizer: optim.Optimizer,
+          criterion: nn.Module,
+          clip: float):
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.train()
+    epoch_loss = 0
+
+    for _, (src, trg) in enumerate(iterator):
+        src, trg = src.to(device), trg.to(device)
+
+        optimizer.zero_grad()
+
+        output = model(src, trg)
+
+        output = output[1:].view(-1, output.shape[-1])
+        trg = trg[1:].view(-1)
+
+        loss = criterion(output, trg)
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
+
+def evaluate(model: nn.Module,
+             iterator: torch.utils.data.DataLoader,
+             criterion: nn.Module):
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.eval()
+
+    epoch_loss = 0
+
+    with torch.no_grad():
+
+        for _, (src, trg) in enumerate(iterator):
+            src, trg = src.to(device), trg.to(device)
+
+            output = model(src, trg, 0) #turn off teacher forcing
+
+            output = output[1:].view(-1, output.shape[-1])
+            trg = trg[1:].view(-1)
+
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
