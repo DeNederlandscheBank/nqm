7e768b2dabe052b52396442a51116e9b156f6124
jm-glowienke <jmglowienke@gmail.com>
2021-03-29 15:13:33 +0200

    small fixes to variable names

--- src_eiopa/features/_build_fairseq_dataset.sh
+++ src_eiopa/features/_build_fairseq_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH=../../data/eiopa/4_dictionaries
-FILE=$PATH/data_24-03_14-14_31181
+DIRECTORY=../../data/eiopa/4_dictionaries
+FILE=$DIRECTORY/data_24-03_14-14_31181
 
-/Users/jan_marcglowienke/anaconda3/envs/nqm_torch/bin/fairseq-preprocess -s nl -t ql --trainpref $FILE-train --validpref $FILE-val --testpref $FILE-test_1 --destdir $PATH/fairseq-data-bin --joined-dictionary
+fairseq-preprocess -s nl -t ql --trainpref $FILE-train --validpref $FILE-val --testpref $FILE-test_1 --destdir $DIRECTORY/fairseq-data-bin --joined-dictionary
