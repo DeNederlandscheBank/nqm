1f36e43a6e9184df9aa814dac904a422c7457992
jm-glowienke <jmglowienke@gmail.com>
2021-03-29 12:05:33 +0200

    fairseq preprocessing working

--- src_eiopa/features/_build_fairseq_dataset.sh
+++ src_eiopa/features/_build_fairseq_dataset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-PATH=../../data/eiopa/4_dictionaries/data_24-03_14-14_31181
+PATH=../../data/eiopa/4_dictionaries
+FILE=$PATH/data_24-03_14-14_31181
 
-fairseq-preprocess -s en -t sparql --trainpref $PATH-train --validpref $PATH-dev --testpref $PATH-test_1 --destdir $PATH/fairseq-data-bin --joined-dictionary
+/Users/jan_marcglowienke/anaconda3/envs/nqm_torch/bin/fairseq-preprocess -s nl -t ql --trainpref $FILE-train --validpref $FILE-val --testpref $FILE-test_1 --destdir $PATH/fairseq-data-bin --joined-dictionary
