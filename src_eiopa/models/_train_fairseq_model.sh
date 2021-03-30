5e08caeccd965291322d20f2d4616dcb9d483908
jm-glowienke <jmglowienke@gmail.com>
2021-03-30 09:57:02 +0200

    status save

--- src_eiopa/models/_train_fairseq_model.sh
+++ src_eiopa/models/_train_fairseq_model.sh
@@ -1,18 +1,23 @@
 # #!/bin/bash
 #
 DATA_DIR=../../data/eiopa/4_dictionaries
-CHECK_DIR=../../models/checkpoints/bert-3118
+MODEL_DIR=../../models
 
-mkdir -p $CHECK_DIR
+mkdir -p $MODEL_DIR/checkpoints
 
-CUDA_VISIBLE_DEVICES=0 fairseq-train \
-    $DATA_DIR/fairseq-data-bin --cpu \
-    --arch transformer --save-dir checkpoints/fconv \
-    --lr 0.25 --clip-norm 0.1 --dropout 0.2 --max-tokens 4000 \
-    --optimizer adam
+# CUDA_VISIBLE_DEVICES=0 fairseq-train \
+#     $DATA_DIR/fairseq-data-bin --cpu \
+#     --arch transformer --save-dir $MODEL_DIR/checkpoints/transformer \
+#     --lr 0.25 --clip-norm 0.1 --dropout 0.2 --max-tokens 4000 \
+#     --optimizer adam \
 
 # mkdir -p ../features/example/checkpoints/fconv
 # CUDA_VISIBLE_DEVICES=0 fairseq-train ../features/example/iwslt14.tokenized.de-en \
 #     --lr 0.25 --clip-norm 0.1 --dropout 0.2 --max-tokens 4000 \
 #     --arch fconv_iwslt_de_en --save-dir checkpoints/fconv \
 #     --source-lang de --target-lang en
+
+fairseq-train \
+    $DATA_DIR/fairseq-data-bin --cpu \
+    --save-dir $MODEL_DIR/checkpoints/wmt16 \
+    --finetune-from-model $MODEL_DIR/wmt16.en-de.joined-dict.transformer/model.pt
