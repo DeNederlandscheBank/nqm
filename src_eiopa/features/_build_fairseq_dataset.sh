88a08410bf0d9b5a20dc1e1d22c947462c6a6074
jm-glowienke <jmglowienke@gmail.com>
2021-03-30 14:17:11 +0200

    iwslt transformer working

--- src_eiopa/features/_build_fairseq_dataset.sh
+++ src_eiopa/features/_build_fairseq_dataset.sh
@@ -3,4 +3,4 @@
 DIRECTORY=../../data/eiopa/4_dictionaries
 FILE=$DIRECTORY/data_24-03_14-14_31181
 
-fairseq-preprocess -s nl -t ql --trainpref $FILE-train --validpref $FILE-val --testpref $FILE-test_1 --destdir $DIRECTORY/fairseq-data-bin --joined-dictionary
+fairseq-preprocess -s nl -t ql --trainpref $FILE-train --validpref $FILE-val --testpref $FILE-test_1 --destdir $DIRECTORY/fairseq-data-bin
--- src_eiopa/models/_train_fairseq_model.sh
+++ src_eiopa/models/_train_fairseq_model.sh
@@ -17,7 +17,11 @@ mkdir -p $MODEL_DIR/checkpoints
 #     --arch fconv_iwslt_de_en --save-dir checkpoints/fconv \
 #     --source-lang de --target-lang en
 
-fairseq-train \
-    $DATA_DIR/fairseq-data-bin --cpu \
-    --save-dir $MODEL_DIR/checkpoints/wmt16 \
-    --finetune-from-model $MODEL_DIR/wmt16.en-de.joined-dict.transformer/model.pt
+fairseq-train $DATA_DIR/fairseq-data-bin \
+-a transformer_iwslt_de_en --optimizer adam --lr 0.0005 -s nl -t ql \
+--label-smoothing 0.1 --dropout 0.3 --max-tokens 4000 \
+--min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
+--criterion label_smoothed_cross_entropy \
+--warmup-updates 4000 --warmup-init-lr '1e-07' \
+--max-epoch 500 --save-interval 100 --valid-subset valid,test \
+--adam-betas '(0.9, 0.98)' --save-dir $MODEL_DIR/transformer_iwslt_de_en
