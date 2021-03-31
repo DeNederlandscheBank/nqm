cadd2effa400f2bc60bd6398fa6d535adb6a5944
jm-glowienke <jmglowienke@gmail.com>
2021-03-31 12:03:29 +0200

    cleaned shell script

--- scripts/data_pipeline_eiopa.sh
+++ scripts/data_pipeline_eiopa.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # full pipeline EIOPA that you can run to prepare the data and train the model
 
-cd ..
+cd .. # pipeline is run from root directory of project
 echo "Making directories..."
-mkdir ./data/eiopa/2_interim/logs
-mkdir ./data/eiopa/3_processed/logs
+mkdir -p ./data/eiopa/2_interim/logs
+mkdir -p ./data/eiopa/3_processed/logs
 # mkdir ../data/nqm/processed
+
+DATA_DIR=data/eiopa/1_external # input data location
+OUT_DIR=data/eiopa/3_processed
 
 echo "Generate job ID"
 # RANDOM=$(date +%s%N | cut -b10-19)
@@ -18,27 +21,27 @@ echo $ID
 
 echo 'Generating data (train, validation)...'
 python src_eiopa/features/generator.py \
-  --templates data/eiopa/1_external/templates.csv \
+  --templates $DATA_DIR/templates.csv \
   --output data/eiopa/2_interim --id $ID --type train_val \
-  --graph-data-path data/eiopa/1_external
+  --graph-data-path $DATA_DIR
 echo 'Splitting data intro train and validation...'
 python src_eiopa/features/splitter.py \
   --inputPath  data/eiopa/2_interim/data_$ID \
-  --outputPath data/eiopa/3_processed/data_$ID --split 80
+  --outputPath $OUT_DIR/data_$ID --split 80
 
 echo 'Generating test data...'
 python src_eiopa/features/generator.py \
-  --templates data/eiopa/1_external/templates_test_1.csv \
-  --output data/eiopa/3_processed --id $ID --type test_1 \
-  --graph-data-path data/eiopa/1_external
+  --templates $DATA_DIR/templates_test_1.csv \
+  --output $OUT_DIR --id $ID --type test_1 \
+  --graph-data-path $DATA_DIR
 python src_eiopa/features/generator.py \
-  --templates data/eiopa/1_external/templates_test_2.csv \
-  --output data/eiopa/3_processed --id $ID --type test_2 \
-  --graph-data-path data/eiopa/1_external
+  --templates $DATA_DIR/templates_test_2.csv \
+  --output $OUT_DIR --id $ID --type test_2 \
+  --graph-data-path $DATA_DIR
 python src_eiopa/features/generator.py \
-  --templates data/eiopa/1_external/templates_test_3.csv \
-  --output data/eiopa/3_processed --id $ID --type test_3 \
-  --graph-data-path data/eiopa/1_external
+  --templates $DATA_DIR/templates_test_3.csv \
+  --output $OUT_DIR --id $ID --type test_3 \
+  --graph-data-path $DATA_DIR
 
 cd scripts
 echo 'Done! Thank you for your patience'
