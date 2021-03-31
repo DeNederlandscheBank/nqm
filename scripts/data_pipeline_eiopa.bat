7413cd2b8df86edf13729974bc692365b08cd3a4
jm-glowienke <jmglowienke@gmail.com>
2021-03-31 12:12:24 +0200

    cleaned bat script (not tested)

--- scripts/data_pipeline_eiopa.bat
+++ scripts/data_pipeline_eiopa.bat
@@ -1,6 +1,6 @@
 :: full pipeline EIOPA that you can run to prepare the data and train the model
 
-cd ..
+cd .. ::pipeline is run from root directory of project
 echo "Making directories..."
 mkdir data\eiopa\3_processed\logs
 mkdir data\eiopa\2_interim\logs
@@ -9,22 +9,32 @@ echo "Generate job id"
 :: .bat script has no date added for job id, since that is complicated due to locale dependent time format
 set /a num=$random$
 
+set DATA_DIR=dir data/eiopa/1_external
+set OUT_DIR=dir data/eiopa/3_processed
+
 echo 'Generating data (train, validation)...'
-python src_eiopa/features/generator.py --templates data/eiopa/1_external/templates.csv --output data/eiopa/3_processed --id %num% --graph-data-path data/eiopa/1_external
+python src_eiopa/features/generator.py ^
+ --templates %DATA_DIR%/templates.csv ^
+ --output data/eiopa/2_interim --id %num% ^
+ --graph-data-path %DATA_DIR%
 echo 'Splitting data intro train and validation...'
-python src_eiopa/features/splitter.py --inputPath  data/eiopa/2_interim/data_train_val_%num% --outputPath data/eiopa/3_processed/data_%num% --split 80
+python src_eiopa/features/splitter.py ^
+ --inputPath  data/eiopa/2_interim/data_train_val_%num% ^
+ --outputPath %OUT_DIR%/data_%num% --split 80
 
 echo 'Generating test data...'
-python src_eiopa/features/generator.py --templates data/eiopa/1_external/templates_test_1.csv --output data/eiopa/3_processed --id %num% --type test_1 --graph-data-path data/eiopa/1_external
-python src_eiopa/features/generator.py --templates data/eiopa/1_external/templates_test_2.csv --output data/eiopa/3_processed --id %num% --type test_2 --graph-data-path data/eiopa/1_external
-python src_eiopa/features/generator.py --templates data/eiopa/1_external/templates_test_3.csv --output data/eiopa/3_processed --id %num% --type test_3 --graph-data-path data/eiopa/1_external
-
-:: echo 'Making vocabularies...'
-:: cd ../models
-:: onmt_build_vocab -config train_config.yaml
-
-:: echo 'Training model...'
-:: onmt_train -config train_config.yaml
+python src_eiopa/features/generator.py ^
+ --templates %DATA_DIR%/templates_test_1.csv ^
+ --output %OUT_DIR% --id %num% --type test_1 ^
+ --graph-data-path %DATA_DIR%
+python src_eiopa/features/generator.py ^
+ --templates %DATA_DIR%/templates_test_2.csv ^
+ --output %OUT_DIR% --id %num% --type test_2 ^
+ --graph-data-path %DATA_DIR%
+python src_eiopa/features/generator.py ^
+ --templates %DATA_DIR%/templates_test_3.csv ^
+ --output %OUT_DIR% --id %num% --type test_3 ^
+ --graph-data-path %DATA_DIR%
 
 cd scripts
 echo 'Done! Thank you for your patience'
