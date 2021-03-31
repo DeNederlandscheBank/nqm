ca7efffee9bf7ca3d0503998edc82c8b051755a1
jm-glowienke <jmglowienke@gmail.com>
2021-03-31 20:15:46 +0200

    added ID to fairseq scripts

--- scripts/_build_fairseq_dataset.sh
+++ scripts/_build_fairseq_dataset.sh
@@ -2,5 +2,6 @@
 
 DIRECTORY=../data/eiopa/4_dictionaries
 FILE=$DIRECTORY/data_24-03_14-14_31181
+ID=31181
 
-fairseq-preprocess -s nl -t ql --trainpref $FILE-train --validpref $FILE-val --testpref $FILE-test_1 --destdir $DIRECTORY/fairseq-data-bin
+fairseq-preprocess -s nl -t ql --trainpref $FILE-train --validpref $FILE-val --testpref $FILE-test_1 --destdir $DIRECTORY/fairseq-data-bin-$ID
