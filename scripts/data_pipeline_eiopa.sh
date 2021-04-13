73aa636c14976ec0748c0f979a8ae03ab3d71492
jm-glowienke <jmglowienke@gmail.com>
2021-04-13 16:15:32 +0200

    made for loop independ of number of test_template files

--- scripts/data_pipeline_eiopa.sh
+++ scripts/data_pipeline_eiopa.sh
@@ -12,6 +12,7 @@ DATA_DIR=data/eiopa/1_external # input data location
 OUT_DIR=data/eiopa/3_processed
 INT_DIR=data/eiopa/2_interim
 DICT_DIR=data/eiopa/4_dictionaries
+TEST_TEMPLATES=test_templates
 
 echo "Generate job ID"
 # RANDOM=$(date +%s%N | cut -b10-19)
@@ -35,7 +36,7 @@ echo 'Generating test data...'
 python src_eiopa/features/generator.py \
   --templates $DATA_DIR \
   --output $INT_DIR --id "$ID" --type test \
-  --graph-data-path $DATA_DIR --folder test_templates \
+  --graph-data-path $DATA_DIR --folder $TEST_TEMPLATES \
   --input-language en
 
 echo 'Learning BPE codes using subword_nmt'
@@ -45,9 +46,11 @@ python src_eiopa/subword-nmt/subword_nmt/learn_joint_bpe_and_vocab.py \
   --write-vocabulary $DICT_DIR/$ID-vocab.nl $DICT_DIR/$ID-vocab.ql \
   --symbols 50
 
-# TODO: change for loop to use automatic counter for test_x
-for L in nl ql; do
-    for f in train.$L val.$L test_1.$L test_2.$L test_3.$L; do
+COUNT_TEST=$((`ls -l $DATA_DIR/$TEST_TEMPLATES | wc -l` -1 ))
+
+# Apply bpe to all relevant files
+for L in nl ql; do # both languages
+    for f in train.$L val.$L test_{1..$COUNT_TEST}.$L; do
         echo "apply_bpe.py to ${f}..."
         python src_eiopa/subword-nmt/subword_nmt/apply_bpe.py \
         --codes "$BPE_CODE" \
