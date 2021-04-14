602afa091568575b73c86b091132680af7684b16
jm-glowienke <jmglowienke@gmail.com>
2021-04-14 21:02:42 +0200

    bugfix, slashes added

--- scripts/HPC_fairseq_transformer.sh
+++ scripts/HPC_fairseq_transformer.sh
@@ -33,15 +33,15 @@ mkdir -p $MODEL_DIR/out_$ID
 
 echo "Model training is started"
 $SRC_DIR/fairseq-train $IN_DIR/fairseq-data-bin-$ID \
-  --tensorboard-logdir $MODEL_DIR/out_$ID/
- -a transformer_iwslt_de_en --optimizer adam --lr 0.0005 -s nl -t ql \
- --label-smoothing 0.1 --dropout 0.3 --max-tokens 4000 \
- --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
- --criterion label_smoothed_cross_entropy --scoring bleu \
- --warmup-updates 4000 --warmup-init-lr '1e-07' \
- --max-epoch 2 --save-interval 2 --valid-subset valid \
- --adam-betas '(0.9, 0.98)' --save-dir $MODEL_DIR/transformer_iwslt_de_en_$ID \
- --batch-size 256 --keep-best-checkpoints 1 --patience 50
+  --tensorboard-logdir $MODEL_DIR/out_$ID/ \
+  --arch transformer_iwslt_de_en --optimizer adam --lr 0.0005 -s nl -t ql \
+  --label-smoothing 0.1 --dropout 0.3 --max-tokens 4000 \
+  --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
+  --criterion label_smoothed_cross_entropy --scoring bleu \
+  --warmup-updates 4000 --warmup-init-lr '1e-07' \
+  --max-epoch 2 --save-interval 2 --valid-subset valid \
+  --adam-betas '(0.9, 0.98)' --save-dir $MODEL_DIR/transformer_iwslt_de_en_$ID \
+  --batch-size 256 --keep-best-checkpoints 1 --patience 50 \
   --eval-bleu \
   --eval-bleu-args '{"beam": 5}' \
   --eval-bleu-detok space \
@@ -49,7 +49,7 @@ $SRC_DIR/fairseq-train $IN_DIR/fairseq-data-bin-$ID \
   --best-checkpoint-metric bleu --maximize-best-checkpoint-metric
 
 echo "Generate translations using fairseq-generate"
-fairseq-generate $IN_DIR/fairseq-data-bin-$ID \
+$SRC_DIR/fairseq-generate $IN_DIR/fairseq-data-bin-$ID \
   --gen-subset test \
   --path $MODEL_DIR/checkpoint_best.pt \
   --results-path $MODEL_DIR/out_$ID \
