113577b3ad40599a0809ede8dc086f9f28c06b50
jm-glowienke <jmglowienke@gmail.com>
2021-04-14 16:49:30 +0200

    small adaption

--- scripts/_evaluate_fairseq_model.sh
+++ scripts/_evaluate_fairseq_model.sh
@@ -11,10 +11,11 @@ fairseq-generate $DATA_DIR/fairseq-data-bin-$ID \
   --results-path $MODEL_DIR/out_$ID \
   --beam 5  \
   --batch-size 128 \
-  --scoring bleu
-  #  --remove-bpe
+  --scoring bleu \
+  --remove-bpe
 
 
 python src_eiopa/evaluation/decode_fairseq_output.py \
   --in-file $MODEL_DIR/out_$ID/generate-test.txt \
   --out-file $MODEL_DIR/out_$ID/translated_queries.txt
+
