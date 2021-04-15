4f837510028c82e3cc4399eb80f5f30ba020d81c
jm-glowienke <jmglowienke@gmail.com>
2021-04-15 14:29:12 +0200

    translate_and_query done

--- src_eiopa/translate_and_query.py
+++ src_eiopa/translate_and_query.py
@@ -5,28 +5,36 @@ query.
 """
 
 from fairseq.models.transformer import TransformerModel
-from generator_utils import sparql_decode
+
 from generator import query_database
+from generator_utils import sparql_decode
+
 
 def translate(question, model_data_dir):
     model = TransformerModel.from_pretrained(
-      model_data_dir,
-      checkpoint_file='./bot.pt',
-      data_name_or_path=model_data_dir,
-      bpe='subword_nmt',
-      bpe_codes='./bot-bpe.codes',
-      tokenizer = 'moses'
+        model_data_dir,
+        checkpoint_file='bot.pt',
+        data_name_or_path=model_data_dir,
+        bpe='subword_nmt',
+        bpe_codes=model_data_dir + '/bpe.codes',
+        tokenizer='moses'
     )
     encoded_sparql = model.translate(question)
     decoded_sparql = sparql_decode(encoded_sparql)
-
     return decoded_sparql
 
-def translate_and_query(question,model_data_dir, graph):
+
+def translate_and_query(question, model_data_dir, graph):
     sparql = translate(question, model_data_dir)
 
-    results = query_database(sparql,graph)
+    results = query_database(sparql, graph)
 
     return results
 
-if __name__ = '__main__':
+
+if __name__ == '__main__':
+    from generator import initialize_graph
+
+    g = initialize_graph('data/eiopa/1_external')
+
+    translate_and_query('Where is aegon?', 'models/bot', g)
