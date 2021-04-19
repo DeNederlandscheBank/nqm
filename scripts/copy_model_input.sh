4b015ae5e16a5d55d5ecb699c980fdab9f9c49f1
jm-glowienke <jmglowienke@gmail.com>
2021-04-19 12:01:47 +0200

    fix in count function

--- scripts/copy_model_input.sh
+++ scripts/copy_model_input.sh
@@ -4,13 +4,13 @@
 # This script copies the required model files using $ID and places them in the
 # correct directory. The name is adapated using $ID_new. These two variables
 # have to be adapted to the desired values.
-ID=13-04_18-21_1689
-ID_NEW=1689
+ID=19-04_11-53_663
+ID_NEW=663
 
 DATA_DIR=data/eiopa/3_processed
 VOC_DIR=data/eiopa/4_vocabularies
 TGT_DIR=data/eiopa/5_model_input
-COUNT_TEST=$((`ls -l data/eiopa/1_external/test_templates | wc -l` -1 ))
+COUNT_TEST=$((`ls -l data/eiopa/1_external/test_templates/*.csv | wc -l` -1 ))
 
 
 # Copy language pairs to correct folder
