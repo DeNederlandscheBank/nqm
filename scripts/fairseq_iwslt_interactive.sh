a35eac4d7bb0a6b041baa2d5c38ebee344bf15a2
jm-glowienke <jmglowienke@gmail.com>
2021-04-28 14:07:03 +0200

    fast_align added as submodule, update used model

--- scripts/fairseq_iwslt_interactive.sh
+++ scripts/fairseq_iwslt_interactive.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-DATA_DIR=data/eiopa/5_model_input/fairseq-data-bin-1689
-MODEL_DIR=models/transformer_iwslt_de_en
+DATA_DIR=data/eiopa/5_model_input/fairseq-data-bin-14126
+MODEL_DIR=models/transformer_iwslt_de_en_14126
 
 fairseq-interactive \
-    --path $MODEL_DIR/checkpoint_best.pt $DATA_DIR \
+    --path $MODEL_DIR/checkpoint.best_bleu_81.91.pt $DATA_DIR \
     --beam 5 --source-lang nl --target-lang ql \
+    --print-alignment --replace-unk \
     --tokenizer moses \
-    --bpe subword_nmt --bpe-codes data/eiopa/4_vocabularies/13-04_18-21_1689-bpe.codes
+#    --bpe subword_nmt --bpe-codes data/eiopa/4_vocabularies/20-04_10-53_14126-bpe.codes
