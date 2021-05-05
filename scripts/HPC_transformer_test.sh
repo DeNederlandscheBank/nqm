9b1a77ebea3dc1a8296aa3e5eb185f63184d6245
jm-glowienke <jmglowienke@gmail.com>
2021-05-05 16:47:09 +0200

    more testing

--- scripts/HPC_transformer_test.sh
+++ scripts/HPC_transformer_test.sh
@@ -67,25 +67,25 @@ mkdir -p $MODEL_DIR/out_$ID
 #      done
 #    }
 
-echo "Model training is started"
-$SRC_DIR/fairseq-train $DATA_BIN \
-  --arch transformer_iwslt_de_en --optimizer adam --lr 0.0005 -s nl -t ql \
-  --label-smoothing 0.1 --dropout 0.3 --max-tokens 4000 \
-  --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
-  --criterion label_smoothed_cross_entropy --scoring bleu \
-  --warmup-updates 4000 --warmup-init-lr '1e-07' \
-  --max-epoch 1 --save-interval 30 --valid-subset valid \
-  --adam-betas '(0.9, 0.98)' --save-dir $MODEL_DIR \
-  --batch-size 256 --keep-best-checkpoints 1 --patience 20 \
-  --eval-bleu \
-  --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
-  --eval-bleu-detok space \
-  --eval-bleu-remove-bpe \
-  --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
-  --stop-time-hours 11 --cpu  \
-  --tensorboard-logdir $MODEL_DIR/out_$ID/
+#echo "Model training is started"
+#$SRC_DIR/fairseq-train $DATA_BIN \
+#  --arch transformer_iwslt_de_en --optimizer adam --lr 0.0005 -s nl -t ql \
+#  --label-smoothing 0.1 --dropout 0.3 --max-tokens 4000 \
+#  --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
+#  --criterion label_smoothed_cross_entropy --scoring bleu \
+#  --warmup-updates 4000 --warmup-init-lr '1e-07' \
+#  --max-epoch 1 --save-interval 30 --valid-subset valid \
+#  --adam-betas '(0.9, 0.98)' --save-dir $MODEL_DIR \
+#  --batch-size 256 --keep-best-checkpoints 1 --patience 20 \
+#  --eval-bleu \
+#  --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+#  --eval-bleu-detok space \
+#  --eval-bleu-remove-bpe \
+#  --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
+#  --stop-time-hours 11 --cpu  \
+#  --tensorboard-logdir $MODEL_DIR/out_$ID/
 
-. scripts/_fairseq_evaluation_align.sh HPC NO-BPE
+. scripts/_fairseq_evaluation_align.sh HPC NO-BPE $ID
 
 #for f in test_{1..$COUNT_TEST}; do
 #  echo "Generate translations using fairseq-generate for $f"
