f046ceffc53fce273c3325d78b8c08355224b5df
jm-glowienke <jmglowienke@gmail.com>
2021-05-18 14:53:14 +0200

    added pointer-generator model and script

--- scripts/fairseq_iwslt_interactive.sh
+++ scripts/fairseq_iwslt_interactive.sh
@@ -2,13 +2,14 @@
 
 # This script should be run from root and can be used to check the translation of a particular sentence.
 
-ID=5861
+ID=31684
 ID_MODEL=$ID
 
 MODEL_DIR=models/transformer_iwslt_de_en_$ID_MODEL
 IN_DIR=data/eiopa/5_model_input
 DATA_BIN=$IN_DIR/fairseq-data-bin-$ID
-BPECODES=$IN_DIR/$ID-bpe.codes
+BPECODES=$IN_DIR/data_$ID/$ID-bpe.codes
+#BPECODES=$IN_DIR/$ID-bpe.codes
 
 CHECKPOINT_BEST_BLEU=$(find $MODEL_DIR -name 'checkpoint.best_bleu_*.pt')
 
@@ -18,4 +19,4 @@ fairseq-interactive $DATA_BIN \
     --beam 5 --source-lang nl --target-lang ql \
     --tokenizer moses \
     --print-alignment --replace-unk \
-#    --bpe subword_nmt --bpe-codes $BPECODES \
+    --bpe subword_nmt --bpe-codes $BPECODES
