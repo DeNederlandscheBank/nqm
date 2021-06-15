5cf1541c7feb27d9f612202f3e0252aed899cd66
jm-glowienke <jmglowienke@gmail.com>
2021-06-15 14:48:49 +0200

    missed command for variable

--- scripts/pq_data_eiopa.sh
+++ scripts/pq_data_eiopa.sh
@@ -3,7 +3,7 @@
 # TO BE USED FOR THE POINTER-GENERATOR MODEL
 # Use this script from the root!
 
- # set this variable to YES, if the generated files should be directly copied to the model_input folder
+COPY=NO # set this variable to YES, if the generated files should be directly copied to the model_input folder
 USE_KNOWN_AND_UNKNOWN_NAMES=NO # if NO, all names are treated as unknown
 VOCAB_SIZE=15000
 POSITION_MARKERS=100
